cb26ca72b26311a62c5b6ae398c34226c3d48bb7
Weddingcompare <daveyhicks@hotmail.co.uk>
2025-04-22 18:55:53 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,8 +1,7 @@
 document.querySelectorAll('.tab-button').forEach(button => {
   button.addEventListener('click', () => {
     document.querySelectorAll('.tab-button').forEach(btn => btn.classList.remove('active'));
-    document.querySelectorAll('.tab-content').forEach(content => content.classList.add('hidden'));
-
+    document.querySelectorAll('.tab-content').forEach(tab => tab.classList.add('hidden'));
     button.classList.add('active');
     document.getElementById(button.dataset.tab).classList.remove('hidden');
   });
