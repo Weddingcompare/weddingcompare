b3e22f5f4d78e28e7156ccc5a530aec09699fa51
Weddingcompare <daveyhicks@hotmail.co.uk>
2025-04-22 15:27:55 +0100

    Delete wedding-venue-updated directory

--- wedding-venue-updated/script.js
+++
@@ -1,6 +0,0 @@
-// script.js placeholder
-document.getElementById("toggleAdvanced").addEventListener("click", () => {
-  document.getElementById("advancedSearch").classList.toggle("hidden");
-});
-
-// Actual venue search/filter code should go here
