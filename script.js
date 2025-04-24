f9e1bac7faca0cf2901ddcd244f3f28e1562906d
Weddingcompare <daveyhicks@hotmail.co.uk>
2025-04-24 10:28:25 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-// JS to be written after HTML
+
+document.getElementById("venueSearchForm").addEventListener("submit", function (e) {
+  e.preventDefault();
+  if (!selectedPlaceCoords) {
+    alert("Please select a valid location.");
+    return;
+  }
+  console.log("Search submitted with location:", selectedPlaceCoords);
+  // This is where filtering logic would go.
+});
